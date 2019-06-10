8f1e24e57baa8bdeafc3079516af58f6775affef
Jhosua Saut Maruli <49725122+LuciferXtools@users.noreply.github.com>
2019-06-10 10:54:02 +0700

    Add files via upload

---
+++ banner.py
@@ -0,0 +1,42 @@
+import os
+import time as jhosua
+
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+
+green = '\033[32;1m'
+
+gta = '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+
+white = '\033[1;37m' # White 
+red = '\033[31m' # red
+orange = '\033[33m' # orange
+blue = '\033[34m' # blue
+p  = '\033[35m' # purple
+C  = '\033[36m' # cyan
+
+
+class banner:
+    def __init__(self):
+        print green+"""
+__________________________________________
+   Created  : Jhosua Saut Maruli
+   Gmail    : lucfaker666@gmail.com
+   Whatsapp : 081260032271
+   Type     : Brute Force
+     Channel You Tube : Mr.F4K3 YT
+     ------------------------------
+__________________________________________ """
+        jhosua.sleep(2)
+        print
+        
+        
+        
+        
+       
